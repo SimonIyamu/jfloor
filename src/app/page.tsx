8b2bb8755133ed5105a8fce456940832a5b01b49
SimonPerisanidis <perisanidis.simon@gmail.com>
2025-09-17 20:18:11 +0300

    cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,17 @@ export default function Home() {
         </p>
       </div>
 
+      {/* CTA */}
+      <div className="mt-12">
+        <a
+          href="https://heavenly-legend-319.notion.site/271641eadaaa805ba57bce75008b646c?pvs=105"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block px-6 py-2 border border-black text-base hover:bg-black hover:text-white transition-colors"
+        >
+          JOIN US
+        </a>
+      </div>
 
       {/* Footer */}
       <footer className="absolute bottom-6 left-0 right-0 text-base text-gray-500 text-center">
